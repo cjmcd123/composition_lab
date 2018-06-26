6cd88f9f1fb7fad9bca4e72a3652111a2522460e
cjmcd123 <cj_mcdowall@btinternet.com>
2018-06-26 11:17:34 +0100

    implemented mouse class

--- src/main/java/device_management/Mouse.java
+++ src/main/java/device_management/Mouse.java
@@ -1,4 +1,28 @@
 package device_management;
 
 public class Mouse {
+
+    private String type;
+    private int buttons;
+
+    public Mouse(String type, int buttons){
+        this.type = type;
+        this.buttons = buttons;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getButtons() {
+        return buttons;
+    }
+
+    public String move(String direction){
+        return "The Mouse is moved: " + direction;
+    }
+
+    public String click(){
+        return "The Mouse clicked the button";
+    }
 }
--- src/test/java/MouseTest.java
+++ src/test/java/MouseTest.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
+import device_management.Mouse;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class MouseTest {
+
+    Mouse mouse;
+
+    @Before
+    public void setUp(){
+        mouse = new Mouse("Wireless", 3);
+    }
+
+    @Test
+    public void mouseHasType(){
+        assertEquals("Wireless", mouse.getType());
+    }
+
+    @Test
+    public void mouseHasButtons(){
+        assertEquals(3, mouse.getButtons());
+    }
+
+    @Test
+    public void mouseCanMove(){
+        assertEquals("The Mouse is moved: right", mouse.move("right"));
+    }
+
+    @Test
+    public void mouseCanClick(){
+        assertEquals("The Mouse clicked the button", mouse.click());
+    }
 }
