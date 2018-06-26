600ff0443cbe7724d5fe3d557a384cdd0f429e68
cjmcd123 <cj_mcdowall@btinternet.com>
2018-06-26 11:25:39 +0100

    added keyboard class

--- src/main/java/device_management/Keyboard.java
+++ src/main/java/device_management/Keyboard.java
@@ -1,4 +1,24 @@
 package device_management;
 
 public class Keyboard {
+
+    private String type;
+    private boolean numPad;
+
+    public Keyboard(String type, boolean numPad){
+        this.type = type;
+        this.numPad = numPad;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public boolean isNumPad() {
+        return numPad;
+    }
+
+    public String type(String data){
+        return "You typed '" + data + "' into the keyboard.";
+    }
 }
--- src/test/java/KeyBoardTest.java
+++ src/test/java/KeyBoardTest.java
@@ -1,2 +1,31 @@
+import device_management.Keyboard;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class KeyBoardTest {
+
+
+    Keyboard keyBoard;
+
+    @Before
+    public void setUp(){
+        keyBoard = new Keyboard("Wireless", true);
+    }
+
+    @Test
+    public void keyboardHasType(){
+        assertEquals("Wireless", keyBoard.getType());
+    }
+
+    @Test
+    public void keyboardHasNumPad(){
+        assertEquals(true, keyBoard.isNumPad());
+    }
+
+    @Test
+    public void keyBoardCanType(){
+        assertEquals("You typed 'Hello World' into the keyboard.", keyBoard.type("Hello World"));
+    }
 }
